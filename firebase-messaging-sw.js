baa19c3e19283fed15578624cdf09c93bd966530
cpmcarshop <paapaapaa810@gmail.com>
2026-09-07 23:14:24 +0900

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,41 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCffFYNOEcrGu2GKgSiE6q6KgGkCtxJEFw",
+  authDomain: "kakuusha-gai.firebaseapp.com",
+  projectId: "kakuusha-gai",
+  storageBucket: "kakuusha-gai.firebasestorage.app",
+  messagingSenderId: "460103021741",
+  appId: "1:460103021741:web:40ed966e261ed657e80224"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notification = payload.notification;
+  const data = payload.data || {};
+  const options = {
+    body: notification.body,
+    icon: '/icons/icon-192x192.png',
+    data: { url: data.click_action || 'https://twitter.com' }
+  };
+  self.registration.showNotification(notification.title, options);
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  const urlToOpen = event.notification.data?.url || 'https://twitter.com';
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((windowClients) => {
+      for (let client of windowClients) {
+        if (client.url === urlToOpen && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(urlToOpen);
+      }
+    })
+  );
+});
